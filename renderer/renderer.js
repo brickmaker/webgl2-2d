8ca3feaa9b3fac57dd68170beceeec89ef40f66e
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-11-30 11:01:28 +0800

    feat: 🎸 renderer basic

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -19,7 +19,8 @@ void main() {
   // convert from 0->2 to -1->+1 (clipspace)
   vec2 clipSpace = zeroToTwo - 1.0;
 
-  gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+//   gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+  gl_Position = vec4(clipSpace, 0, 1);
 }
 `;
 
@@ -62,132 +63,88 @@ class Renderer {
         this.vao = this.gl.createVertexArray();
 
         // and make it the one we're currently working with
-        gl.bindVertexArray(vao);
+        this.gl.bindVertexArray(this.vao);
 
         // Turn on the attribute
-        gl.enableVertexAttribArray(positionAttributeLocation);
+        this.gl.enableVertexAttribArray(this.positionAttributeLocation);
 
         // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+
+        // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
+        var size = 2;          // 2 components per iteration
+        var type = this.gl.FLOAT;   // the data is 32bit floats
+        var normalize = false; // don't normalize the data
+        var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
+        var offset = 0;        // start at the beginning of the buffer
+        this.gl.vertexAttribPointer(this.positionAttributeLocation, size, type, normalize, stride, offset);
+
+        // create the buffer
+        this.indexBuffer = this.gl.createBuffer();
+
+        // make this buffer the current 'ELEMENT_ARRAY_BUFFER'
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.indexBuffer);
+
+
+        // TODO: consider devicePixelRatio
+        // Tell WebGL how to convert from clip space to pixels
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+
+        // Clear the canvas
+        this.gl.clearColor(0, 0, 0, 0);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+        // Tell it to use our program (pair of shaders)
+        this.gl.useProgram(this.program);
+
+        // Bind the attribute/buffer set we want.
+        this.gl.bindVertexArray(this.vao);
+
+        // Pass in the canvas resolution so we can convert from
+        // pixels to clipspace in the shader
+        this.gl.uniform2f(this.resolutionUniformLocation, this.gl.canvas.width, this.gl.canvas.height);
 
     }
-}
 
+    draw(vertices, indices, color) {
 
-function main() {
-    // Get A WebGL context
-    var canvas = document.querySelector("#main");
-    var gl = canvas.getContext("webgl2");
-    if (!gl) {
-        return;
-    }
-
-    // Use our boilerplate utils to compile the shaders and link into a program
-    var program = webglUtils.createProgramFromSources(gl,
-        [vertexShaderSource, fragmentShaderSource]);
-
-    // look up where the vertex data needs to go.
-    var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-    // look up uniform locations
-    var resolutionUniformLocation = gl.getUniformLocation(program, "u_resolution");
-    var colorLocation = gl.getUniformLocation(program, "u_color");
-
-    // Create a buffer
-    var positionBuffer = gl.createBuffer();
-
-    // Create a vertex array object (attribute state)
-    var vao = gl.createVertexArray();
-
-    // and make it the one we're currently working with
-    gl.bindVertexArray(vao);
-
-    // Turn on the attribute
-    gl.enableVertexAttribArray(positionAttributeLocation);
-
-    // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
-    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-    var size = 2;          // 2 components per iteration
-    var type = gl.FLOAT;   // the data is 32bit floats
-    var normalize = false; // don't normalize the data
-    var stride = 0;        // 0 = move forward size * sizeof(type) each iteration to get the next position
-    var offset = 0;        // start at the beginning of the buffer
-    gl.vertexAttribPointer(
-        positionAttributeLocation, size, type, normalize, stride, offset);
-
-    // create the buffer
-    const indexBuffer = gl.createBuffer();
-
-    // make this buffer the current 'ELEMENT_ARRAY_BUFFER'
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-
-    // Fill the current element array buffer with data
-    const indices = [
-        0, 1, 2,   // first triangle
-        2, 1, 3,   // second triangle
-    ];
-    gl.bufferData(
-        gl.ELEMENT_ARRAY_BUFFER,
-        new Uint16Array(indices),
-        gl.STATIC_DRAW
-    );
-
-    webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-    // Tell WebGL how to convert from clip space to pixels
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    // Clear the canvas
-    gl.clearColor(0, 0, 0, 0);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // Tell it to use our program (pair of shaders)
-    gl.useProgram(program);
-
-    // Bind the attribute/buffer set we want.
-    gl.bindVertexArray(vao);
-
-    // Pass in the canvas resolution so we can convert from
-    // pixels to clipspace in the shader
-    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-    // draw 50 random rectangles in random colors
-    for (var ii = 0; ii < 50; ++ii) {
         // Put a rectangle in the position buffer
-        setRectangle(
-            gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+        this.gl.bufferData(
+            this.gl.ARRAY_BUFFER,
+            new Float32Array(vertices),
+            this.gl.STATIC_DRAW);
 
-        // Set a random color.
-        gl.uniform4f(colorLocation, Math.random(), Math.random(), Math.random(), 1);
+        // Fill the current element array buffer with data
+        this.gl.bufferData(
+            this.gl.ELEMENT_ARRAY_BUFFER,
+            new Uint16Array(indices),
+            this.gl.STATIC_DRAW
+        );
+
+        this.gl.uniform4fv(this.colorLocation, new Float32Array(color));
 
         // Draw the rectangle.
-        var primitiveType = gl.TRIANGLES;
+        var primitiveType = this.gl.TRIANGLES;
         var offset = 0;
-        var count = 6;
-        var indexType = gl.UNSIGNED_SHORT;
-        gl.drawElements(primitiveType, count, indexType, offset);
+        var count = indices.length;
+        var indexType = this.gl.UNSIGNED_SHORT;
+        this.gl.drawElements(primitiveType, count, indexType, offset);
     }
 }
 
-// Returns a random integer from 0 to range - 1.
-function randomInt(range) {
-    return Math.floor(Math.random() * range);
-}
+const renderer = new Renderer(document.querySelector('#main'))
 
-// Fill the buffer with the values that define a rectangle.
-function setRectangle(gl, x, y, width, height) {
-    var x1 = x;
-    var x2 = x + width;
-    var y1 = y;
-    var y2 = y + height;
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        x1, y1,  // vertex 0
-        x2, y1,  // vertex 1
-        x1, y2,  // vertex 2
-        x2, y2,  // vertex 3
-    ]), gl.STATIC_DRAW);
-}
+const vertices = [
+    10, 10,
+    100, 30,
+    10, 60,
+    100, 60
+]
+const indices = [
+    0, 1, 2,   // first triangle
+    2, 1, 3,   // second triangle
+];
 
-main();
+const color = [Math.random(), Math.random(), Math.random(), 1]
+
+renderer.draw(vertices, indices, color)
