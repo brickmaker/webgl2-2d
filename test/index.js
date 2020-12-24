77fd4e8ae11c07f83fa8e8902f19f91ee58e5eb5
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-12-24 15:05:49 +0800

    feat: 🎸 clip test

--- test/index.js
+++ test/index.js
@@ -245,6 +245,21 @@ function drawLineCaps(ctx) {
     ctx.stroke()
 }
 
+function drawHeart(ctx) {
+    ctx.beginPath();
+    ctx.moveTo(75, 40);
+    ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+    ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+    ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+    ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+    ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+    ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+    ctx.clip()
+
+    ctx.fillStyle = 'red'
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+}
+
 // performDraw(drawLine)
 // performDraw(drawShape)
 // performDraw(drawRect)
@@ -255,4 +270,5 @@ function drawLineCaps(ctx) {
 // performDraw(pointInPath)
 // performDraw(lineJoins)
 // performDraw(drawSimpleLineJoin)
-performDraw(drawLineCaps)
+// performDraw(drawLineCaps)
+performDraw(drawHeart)
